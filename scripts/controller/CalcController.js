a2836f08e0f7299f0961c0f69f8ec1d9f58617d2
lucasvergara1 <lucas.vergara@live.com>
2023-12-21 23:40:49 -0300

    :construction: Configuration of operator in the buttons

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,102 +1,197 @@
 class CalcController {
-    // Construtor da classe
     constructor() {
-        // Configuração inicial
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
-        
-        // Inicialização da calculadora
         this.initialize();
-        // Configuração de eventos dos botões
         this.initButtonsEvents();
     }
 
-    // Inicialização da calculadora
     initialize() {
-        // Configura um intervalo de 1 segundo para atualizar a data e hora no display
         setInterval(() => {
-            // Chama a função para atualizar data e hora no display
             this.setDisplayDateTime();
         }, 1000);
     }
 
-    // Adiciona múltiplos eventos a um elemento
     addEventListenerAll(element, events, fn) {
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
         });
     }
 
-    // Configura os eventos dos botões da calculadora
+    clearAll(){
+        this._operation= [];
+
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    getLastOperation(){
+
+        return this._operation[this._operation.lenght -1];
+
+    }
+
+    setLastOperation(value){
+
+        this._operation[this._operation.lenght -1] = value;
+    }
+
+    isOperator(){
+
+        return (['+','-','*','%','/'].indexOf(value) > -1);
+
+        }
+
+    }
+
+    addOperation(value){
+
+        console.log('A', isNaN(this.getLastOperation()));
+
+        if (isNaN(this.getLastOperation())) {
+
+            if(this.isOperator(value)) {
+
+                this._setLastOperation(value);
+
+            } else if(isNaN(value)) {
+                
+                console.log(value);
+
+            } else {
+
+                this._operation.push(value);
+
+            }
+
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+        switch(value) {
+            case 'ac';
+                this.clearAll();
+                break;
+            case 'ce';
+                this.clearentry();
+                break;
+            case 'soma';
+            this.addOperation('+');
+                break;
+            case 'subtracao';
+            this.addOperation('-');
+                break;
+            case 'divisao';
+            this.addOperation('/');
+                break;
+            case 'multiplicao';
+            this.addOperation('*');
+                break;
+            case 'porcento';
+            this.addOperation('%');
+                break;
+            case 'igual';
+
+                break;
+
+            case 'ponto';
+            this.addOperation('.');
+            break;
+
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                this.addOperation(parseInt(value));
+                break;
+
+
+            default:
+                this.setError();
+                break;    
+
+        }
+    }
+
     initButtonsEvents() {
-        // Seleciona todos os botões da calculadora
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
-        // Para cada botão, configura eventos de clique e arrastar
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, "click drag", e => {
-                // Exibe no console a classe do botão, removendo o prefixo "btn-"
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+                this.execBtn(textBtn);
             });
 
-            // Configura eventos de mouseover, mouseup e mousedown para mostrar o cursor como ponteiro
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
                 btn.style.cursor = "pointer";
             });
         });
     }
 
-    // Atualiza a data e hora no display
     setDisplayDateTime() {
-        // Formata a data e atualiza o display
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: "2-digit",
             month: "long",
             year: "numeric",
         });
-        // Atualiza a hora no display
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
-    // Obtém a hora do display
     get displayTime() {
         return this._timeEl.innerHTML;
     }
 
-    // Define a hora no display
     set displayTime(value) {
-        return this._timeEl.innerHTML = value;
+        this._timeEl.innerHTML = value;
     }
 
-    // Obtém a data do display
     get displayDate() {
         return this._dateEl.innerHTML;
     }
 
-    // Define a data no display
     set displayDate(value) {
-        return this._dateEl.innerHTML = value;
+        this._dateEl.innerHTML = value;
     }
 
-    // Obtém o valor atual no display da calculadora
     get displayCalc() {
         return this._displayCalcEl.innerHTML;
     }
 
-    // Define o valor no display da calculadora
     set displayCalc(value) {
         this._displayCalcEl.innerHTML = value;
     }
 
-    // Obtém a data atual
     get currentDate() {
         return new Date();
     }
 
-    // Define a data atual
     set currentDate(value) {
         this.currentDate = value;
     }
