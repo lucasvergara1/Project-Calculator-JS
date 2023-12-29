9d45af5704150837cf349375131989087fed6970
lucasvergara1 <lucas.vergara@live.com>
2023-12-29 01:25:49 -0300

    :construction: Create % operation

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -23,6 +23,8 @@ class CalcController {
 
         }, 1000);
 
+        this.setLastNumberToDisplay();
+
     }
 
     addEventListenerAll(element, events, fn){
@@ -39,11 +41,15 @@ class CalcController {
 
         this._operation = [];
 
+        this.setLastNumberToDisplay();
+
     }
 
     clearEntry(){
 
         this._operation.pop();
+
+        this.setLastNumberToDisplay();
 
     }
 
@@ -79,11 +85,29 @@ class CalcController {
 
     calc(){
 
-        let last = this._operation.pop();
+        let last = '';
+
+        if (this._operation.length > 3) {
+
+            last = this._operation.pop();
+
+        }
         
         let result = eval(this._operation.join(""));
 
-        this._operation = [result, last];
+        if (last == '%') {
+
+            result /= 100;
+
+            this._operation = [result];
+
+        } else {
+
+            this._operation = [result];
+
+            if (last) this._operation.push(last);
+
+        }
 
         this.setLastNumberToDisplay();
 
@@ -104,6 +128,8 @@ class CalcController {
             }
 
         }
+
+        if (!lastNumber) lastNumber = 0;
 
         this.displayCalc = lastNumber;
 
@@ -189,7 +215,7 @@ class CalcController {
                 break;
 
             case 'igual':
-                
+                this.calc();
                 break;
 
             case 'ponto':
